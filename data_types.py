1ab4827e127933fc0294cf73ce02530a42d7c1ee
Divakar Reddy Doma <Divakar.Reddy.Doma@kyndryl.com>
2025-10-26 19:48:38 +0530

    first commit

---
+++ data_types.py
@@ -0,0 +1,14 @@
+"""
+1. What is a Data Type?
+
+A data type defines the kind of value a variable can hold and the operations that can be performed on it.
+Python is dynamically typed, so you don’t declare the type explicitly — it’s assigned automatically at runtime.
+"""
+
+
+x = 10       # int
+y = "Hello Divakar"  # str
+z = 3.14     # float
+print(type(x), type(y), type(z))
+
+
